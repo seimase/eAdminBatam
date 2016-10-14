e9c019d491826e3e28c9da035dd330acd88594de
setia <nugraha.setia@gmail.com>
2016-10-14 16:16:21 +0700

    Add fcm

--- E-Admin/app/src/main/java/fcm/MyAndroidFirebaseMsgService.java
+++ E-Admin/app/src/main/java/fcm/MyAndroidFirebaseMsgService.java
@@ -10,6 +10,7 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.bpbatam.eadmin.MainActivity;
+import com.bpbatam.eadmin.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
